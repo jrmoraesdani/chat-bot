a28dd0408545c6bfc70984c64c65b3e170bf4531
Daniel Moraes <jrmoraesdaniel@gmail.com>
2024-09-18 16:59:57 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,33 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'self' *.bubbleapps.io;
+    upgrade-insecure-requests;
+`
+ 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
